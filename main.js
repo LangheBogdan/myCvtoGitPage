a51c2aa86612d0485fa47a69c90950ccee63adae
Bogdan Langhe <bogdanlanghe@gmail.com>
2021-12-28 02:16:33 +0200

    Add js function to style all level two headings

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert('Work in progress');
+// Selects all level two headings
+const headingLevelTwo = document.querySelectorAll('h2');
+
+// Add class to all level two headings
+const styleHeadingTwo = element => element.classList.add('headingLevelTwo');
+
+headingLevelTwo.forEach(styleHeadingTwo);
