ab5df3a88a5734ef7ea25ef7ecd6cb426553a52c
Bogdan Langhe <bogdanlanghe@gmail.com>
2021-12-29 02:22:54 +0200

    add more styles

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-alert('Work in progress');
+// TODO Enable alert;
+// alert('Work in progress');
 // Selects all level two headings
-
 const headingLevelTwo = document.querySelectorAll('h2');
 
-//Selects all level three headings
+// Select all <li></li>
+const listLi = document.querySelectorAll('li');
 
+//Selects all level three headings
 const headingLevelThree = document.querySelectorAll('h3');
 
 //Select all level four headings
-
 const headingLevelFour = document.querySelectorAll('h4');
 
 // Add class to all level two headings
-
 const styleHeadingTwo = element => element.classList.add('headingLevelTwo');
 headingLevelTwo.forEach(styleHeadingTwo);
 
 //Add class to all level three headings
-
 const styleHeadingThree = element => element.classList.add('headingLevelThree');
 headingLevelThree.forEach(styleHeadingThree);
 
 // Add class to all level four headings
 const styleHeadingFour = element => element.classList.add('headingLevelFour');
 headingLevelFour.forEach(styleHeadingFour);
+
+const styleLi = element => element.classList.add('listLi');
+listLi.forEach(styleLi);
